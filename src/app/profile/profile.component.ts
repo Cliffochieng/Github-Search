86bb20bf47a48697c0e7164bdba6d54fa73e5a74
Cliffochieng <cliffordochieng844@gmail.com>
2020-10-03 17:48:05 +0300

    add GitSearchService class

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -4,7 +4,9 @@ import { GitSearchService } from '../git-search.service';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
+  providers:[GitSearchService]
+  
 })
 export class ProfileComponent implements OnInit {
 
