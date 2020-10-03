836a7939929b59ba6d7f1a65595caf28ff288371
Cliffochieng <cliffordochieng844@gmail.com>
2020-10-03 18:36:22 +0300

    import Httpclient and Git-Search to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,16 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 import { RoutingModule} from './routing/routing.module';
 import { HighlightDirective } from './highlight.directive';
 import { JoinDateCountPipe } from './join-date-count.pipe';
+import { GitSearchService } from './git-search.service';
 import { RepoComponent } from './repo/repo.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
 
 @NgModule({
   declarations: [
@@ -24,8 +27,10 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    
   ],
-  providers: [],
+  providers: [GitSearchService],
   bootstrap: [AppComponent],
   exports: [HighlightDirective, JoinDateCountPipe]
 })
