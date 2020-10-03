4eaa24be03f2205d6413e9d65d9f63953b1529b7
Cliffochieng <cliffordochieng844@gmail.com>
2020-10-03 18:00:25 +0300

    worked on the ui and buss logic in repo.component.html

--- src/app/git-search.service.ts
+++ src/app/git-search.service.ts
@@ -4,6 +4,13 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class GitSearchService {
+  profile: any;
+  updateProfile(username: string) {
+    throw new Error("Method not implemented.");
+  }
+  profileRequest() {
+    throw new Error("Method not implemented.");
+  }
 
   constructor() { }
 }
