336a313967df387519adb4620399c0f82c4b4581
Cliffochieng <cliffordochieng844@gmail.com>
2020-10-03 18:10:45 +0300

    add username and repo on export class

--- src/app/repo/repo.component.ts
+++ src/app/repo/repo.component.ts
@@ -7,10 +7,13 @@ import { Repo } from '../repo';
 @Component({
   selector: 'app-repo',
   templateUrl: './repo.component.html',
-  styleUrls: ['./repo.component.css']
+  styleUrls: ['./repo.component.css'],
+  providers:[GitSearchService],
 })
 export class RepoComponent implements OnInit {
-
+  username:string;
+  repo:Repo;
+  
   constructor() { }
 
   ngOnInit(): void {
