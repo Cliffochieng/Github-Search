e5d4a3d47388e34b453a8cf5f6435bbe3ebe1822
Cliffochieng <cliffordochieng844@gmail.com>
2020-10-03 16:54:42 +0300

    generated repo component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { RoutingComponent } from './routing/routing.component';
 import { HighlightDirective } from './highlight.directive';
 import { JoinDateCountPipe } from './join-date-count.pipe';
+import { RepoComponent } from './repo/repo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RoutingComponent,
     HighlightDirective,
-    JoinDateCountPipe
+    JoinDateCountPipe,
+    RepoComponent
   ],
   imports: [
     BrowserModule,
