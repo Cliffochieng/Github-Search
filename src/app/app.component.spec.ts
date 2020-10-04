21e72117df9287903462a87695ca3ae3f716ff24
Cliffochieng <cliffordochieng844@gmail.com>
2020-10-04 18:10:19 +0300

    rectifying all th errors

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,10 +13,10 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'github-search-app'`, async(() => {
+  it(`should have as title 'Github-search'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('github-search-app');
+    expect(app.title).toEqual('Github-search');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
